d6114e18924b160b028e5cbbf61a274cfdd3602d
VitorBsdc <vitor.bsdc@gmail.com>
2024-02-21 17:17:23 -0300

    base project finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "James Rodriguez fica no SPFC",
+    "description": "Após dias turbulentos, James Rodriguez decide permanecer no SPFC e pede desculpas a equipe, nesta segunda feira após reunião com dirigentes, o clube e o jogador chegou ao acordo da permanência do jogador, visando a sua recuperação no clube.",
+    "photoCover": "https://tntsports.com.br/__export/1707322516353/sites/esporteinterativo/img/2024/02/07/20240207-141030_1971632_2871.jpg_1758632412.jpg"
+  },
+  {
+    "id": "2",
+    "title": "SPFC desmente sobre Lamela",
+    "description": "Após jornal espanhol afirmar procura do SPFC por Eric Lamela, a equipe desmente o fato e diz não ter procurado o jogador para negociações, e possível substituição de James Rodriguez.",  
+    "photoCover": "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2024/02/20/1852114113-lamela.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Carpini conta com volta de jogadores do DM",
+    "description": "Em semana livre por conta da primeira fase da Copa do Brasil e sem o Paulistão, o técnico Thiago Carpini pode contar com volta de jogadores do departamento médico, sendo eles: Wellignton Rato, Rafinha, Igor Vinicius, Moreira, Nestor, Rodriguinho, Lucas Moura, Erick e Michel Araújo.",  
+    "photoCover": "https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2024%2F0121%2Fr1280151_1296x729_16%2D9.jpg"
+  },
+  {
+    "id": "4",
+    "title": "SPFC amarga a 3° colocação no grupo D do paulistão",
+    "description": "Após mais um resultado ruim contra o RB Bragantino e com a vitória do São Bernardo contra o Santo André, São Paulo amarga a 3° colocação do grupo restando apenas 3 rodadas da fase de gruops.",  
+    "photoCover": "https://media.torcedores.com/wp-content/uploads/2024/02/spfc-jejum-paulistao.jpg"
+  }
+]
